1f3b8b6c8ba1cc0f1751ce213748fe50bbe41f58
CarlHarlang <carl856k@stud.kea.dk>
2022-11-14 13:21:46 +0100

    Food type and Eat added

--- src/Cat.java
+++ src/Cat.java
@@ -1,11 +1,11 @@
 public class Cat extends Animal{
-    @Override
+
     public void makeSound() {
         System.out.println("Miav");
 
     }
 
-    @Override
+
     public boolean eat(String foodtype) {
         return true;
     }
--- src/Dog.java
+++ src/Dog.java
@@ -1,10 +1,10 @@
 public class Dog extends Animal {
-    @Override
+
     public void makeSound() {
         System.out.println("Vuf");
     }
 
-    @Override
+
     public boolean eat(String foodtype) {
         return true;
     }
